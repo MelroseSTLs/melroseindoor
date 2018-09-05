1d27fa2e3b762ca59bf70ec8c10a40ace238af97
jmurphy192 <32463365+jmurphy192@users.noreply.github.com>
2018-09-05 10:57:46 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,49 @@
+// Async HTTP GET Function
+var HttpClient = function() { // Thanks http://stackoverflow.com/a/22076667/1709894!
+  this.get = function(aUrl, aCallback) {
+    var anHttpRequest = new XMLHttpRequest();
+    anHttpRequest.onreadystatechange = function() {
+      if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+        aCallback(anHttpRequest.responseText);
+    }
+
+    anHttpRequest.open( "GET", aUrl, true );
+    anHttpRequest.send( null );
+  }
+}
+
+var getCsvSheet = new HttpClient();
+getCsvSheet.get("https://cors-anywhere.herokuapp.com/https://docs.google.com/spreadsheet/ccc?key=1S4dX2zBJ0yoEeJ88SOzb4O9Jxxj-TKMZYtxy45dCTsM&output=csv", function(response) {
+	// Parse local CSV file
+	Papa.parse(response, {
+		complete: function(results) {
+			console.log("Finished:", results.data);
+			var myString = '';
+			for (x in results.data) {
+				if(x != 0 && results.data[x][1].length > 0){
+					myString += "<li>"+results.data[x][1]+"</li>";
+				}
+			}
+			changeText(myString);
+			console.log(myString);
+		}
+	});
+});
+
+function main(){
+	console.log("Loaded");
+}
+
+function changeText(text){
+	var elem = document.getElementById("wnlist");
+	elem.innerHTML = "<ul id='wnlist'>"+text+"</ul>";
+}
+
+function openNav(){
+	document.getElementById("sidenav").style.width = "30%";
+}
+
+function closeNav() {
+	document.getElementById("sidenav").style.width = "0";
+}
 
